748d08f34b4719d325f75ffb3168333162680794
marcus_rk <makj0005@stud.kea.dk>
2024-10-19 15:55:45 +0200

    Added env for API url in gateway.py

--- microservice/api_gateway/gateway.py
+++ microservice/api_gateway/gateway.py
@@ -1,26 +1,30 @@
 from flask import Flask, jsonify, request
-import requests
+import requests, os
+
+CURRENCY_SERVICE_URL = os.getenv('CURRENCY_SERVICE_URL')
+PRODUCT_SERVICE_URL = os.getenv('PRODUCT_SERVICE_URL')
+INVENTORY_SERVICE_URL = os.getenv('INVENTORY_SERVICE_URL')
 
 app = Flask(__name__)
 
 # Routes for Currency Service
-@app.route('/currency/<currency>/<price>', methods=['GET'])
+@app.route('/currency/<string:currency>/<int:price>', methods=['GET'])
 def currency_service(currency, price):
-    currency_url = f"http://currency_service:5003/currency/{currency}/{price}"
+    currency_url = f'{CURRENCY_SERVICE_URL}/currency/{currency}/{price}'
     response = requests.get(currency_url)
     return jsonify(response.json())
 
 # Routes for Product Service
-@app.route('/product/<int:id>', methods=['GET'])
+@app.route('/products/<int:id>', methods=['GET'])
 def product_service(id):
-    product_url = f"http://product_service:5002/product/{id}"
+    product_url = f'{PRODUCT_SERVICE_URL}/products/{id}'
     response = requests.get(product_url)
     return jsonify(response.json())
 
 # Routes for Inventory Service
 @app.route('/inventory/<int:product_id>', methods=['GET'])
 def inventory_service(product_id):
-    inventory_url = f"http://inventory_service:5001/inventory/{product_id}"
+    inventory_url = f'{INVENTORY_SERVICE_URL}/stock/{product_id}'
     response = requests.get(inventory_url)
     return jsonify(response.json())
 
